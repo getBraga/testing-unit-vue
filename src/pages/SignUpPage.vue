0a2d63bd4be082e62e06f11a83815a5ffab9b3e5
Ysmael Braga <ysmael.braga@ibm.com>
2022-08-31 00:00:55 -0300

    testando o formulario do SignUpPage.vue

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <h1>Sign Up </h1>
+    <h1>Sign Up</h1>
+    <label for="username">Username</label>
+    <input id="username" />,
+    <label for="e-mail">E-mail</label>
+    <input id="e-mail" />
+    <label for="password">Password</label>
+    <input id="password" type="password" />
+    <label for="password">Password Repeat</label>
+    <input id="password" type="password" />
+    <button disabled>Sign Up</button>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
