cc89cca5fc313040c60d660db5921ca76085836d
Ysmael Braga <ysmael.braga@ibm.com>
2022-09-01 22:51:39 -0300

    password e password repeat inseridos para desabilitar e habilitar o botao de sign up

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,20 +1,36 @@
+<!-- eslint-disable vue/no-side-effects-in-computed-properties -->
 <template>
   <div>
     <h1>Sign Up</h1>
+
     <label for="username">Username</label>
     <input id="username" />,
     <label for="e-mail">E-mail</label>
     <input id="e-mail" />
     <label for="password">Password</label>
-    <input id="password" type="password" />
-    <label for="password">Password Repeat</label>
-    <input id="password" type="password" />
-    <button disabled>Sign Up</button>
+    <input id="password" type="password" v-model="password" />
+    <label for="password-repeat">Password Repeat</label>
+    <input id="password-repeat" type="password" v-model="passwordRepeat" />
+    <button :disabled="btnDisabled">Sign Up</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      password: "",
+      passwordRepeat: "",
+    };
+  },
+  computed: {
+    btnDisabled() {
+      return this.password.length && this.passwordRepeat.length
+        ? this.password !== this.passwordRepeat
+        : true;
+    },
+  },
+};
 </script>
 
 <style></style>
