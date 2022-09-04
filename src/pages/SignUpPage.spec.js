228056a865bb4a0dd9d51dc9fc2ca8eecc067b4e
Ysmael Braga <ysmaelbraga01@gmail.com>
2022-09-04 00:56:34 -0300

    mockando o servico backend para simular uma requisicao de usuario

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,5 +1,6 @@
 import SignUpPage from "./SignUpPage.vue";
-
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 import { render, screen } from "@testing-library/vue";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
@@ -56,13 +57,48 @@ describe("Sign Up Page", () => {
   describe("Interactions", () => {
     it("enables the button when the password and password repeat fields have same value", async () => {
       render(SignUpPage);
+
       const passwordInput = screen.queryByLabelText("Password");
       const passwordInutRepeat = screen.queryByLabelText("Password Repeat");
+
       await userEvent.type(passwordInput, "P4ssword");
       await userEvent.type(passwordInutRepeat, "P4ssword");
 
       const button = screen.queryByRole("button", { name: "Sign Up" });
+
       expect(button).toBeEnabled();
+    });
+    it("sends username, email and password to backend after clicking the button", async () => {
+      let requestBody;
+      const server = setupServer(
+        rest.post("/api/1.0/users", async (req, res, ctx) => {
+          requestBody = await req.json();
+          return res(ctx.status(200));
+        })
+      );
+      server.listen();
+      render(SignUpPage);
+      const usernameInput = screen.queryByLabelText("Username");
+      const emailInput = screen.queryByLabelText("E-mail");
+      const passwordInput = screen.queryByLabelText("Password");
+      const passwordInutRepeat = screen.queryByLabelText("Password Repeat");
+      await userEvent.type(usernameInput, "User1");
+      await userEvent.type(emailInput, "user1@mail.com");
+      await userEvent.type(passwordInput, "P4ssword");
+      await userEvent.type(passwordInutRepeat, "P4ssword");
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      // const mockFn = jest.fn();
+      // window.fetch = mockFn;
+      await userEvent.click(button);
+      server.close();
+      // const [firstCall] = mockFn.mock.calls;
+      // const body = JSON.parse(firstCall[1].body);
+
+      expect(requestBody).toEqual({
+        username: "User1",
+        email: "user1@mail.com",
+        password: "P4ssword",
+      });
     });
   });
 });
