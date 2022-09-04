1393b15a6dfe63c58da417343e207e972ffab4b8
Ysmael Braga <ysmaelbraga01@gmail.com>
2022-09-04 00:57:04 -0300

    incluido o metodo para enviar as informacoes para o backend

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,24 +1,32 @@
 <!-- eslint-disable vue/no-side-effects-in-computed-properties -->
 <template>
-  <div>
+  <form>
     <h1>Sign Up</h1>
 
     <label for="username">Username</label>
-    <input id="username" />,
+    <input id="username" v-model="username" />
     <label for="e-mail">E-mail</label>
-    <input id="e-mail" />
+    <input id="e-mail" v-model="email" />
     <label for="password">Password</label>
-    <input id="password" type="password" v-model="password" />
+    <input id="password" type="password" v-model="password" autocomplete="on" />
     <label for="password-repeat">Password Repeat</label>
-    <input id="password-repeat" type="password" v-model="passwordRepeat" />
-    <button :disabled="btnDisabled">Sign Up</button>
-  </div>
+    <input
+      id="password-repeat"
+      type="password"
+      v-model="passwordRepeat"
+      autocomplete="on"
+    />
+    <button :disabled="btnDisabled" @click.prevent="submit">Sign Up</button>
+  </form>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
+      username: "",
+      email: "",
       password: "",
       passwordRepeat: "",
     };
@@ -28,6 +36,15 @@ export default {
       return this.password.length && this.passwordRepeat.length
         ? this.password !== this.passwordRepeat
         : true;
+    },
+  },
+  methods: {
+    submit() {
+      axios.post("/api/1.0/users", {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
     },
   },
 };
