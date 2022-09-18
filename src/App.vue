a1885bd3a4b4f47dc47e01e0a023122adbce6d23
Ysmael Braga <ysmael.braga@ibm.com>
2022-09-18 16:53:39 -0300

    included the homepage and LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="container"><SignUpPage /> <LanguageSelector /></div>
+  <div class="container">
+    <HomePage v-if="path === '/'" />
+    <SignUpPage v-else-if="path === '/signup'" />
+    <LoginPage v-else-if="path === '/login'" />
+    <LanguageSelector />
+  </div>
 </template>
 
 <script>
 import SignUpPage from "./pages/SignUpPage.vue";
+import HomePage from "./pages/HomePage.vue";
+import LoginPage from "./pages/LoginPage.vue";
 import LanguageSelector from "./components/LanguageSelector.vue";
 export default {
   name: "App",
   components: {
     SignUpPage,
     LanguageSelector,
+    HomePage,
+    LoginPage,
+  },
+  computed: {
+    path() {
+      return window.location.pathname;
+    },
   },
 };
 </script>
